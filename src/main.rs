91a814e625cbdd4744d7e8307d96bc6cf2cbf61e
Christos Siannas <xrsia@otenet.gr>
2026-04-21 14:43:59 +0300

    filter only pdf files

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use walkdir::WalkDir;
 fn main() {
     for entry in WalkDir::new("./test_folder") {
         let entry = entry.unwrap();
-        println!("{:?}", entry.path());
+        let path = entry.path();
+
+        let path_str = path.to_string_lossy();
+
+        if path_str.ends_with(".pdf") {
+            println!("PDF: {:?}", path);
+        }
     }
 }
