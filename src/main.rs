55ec945305e002e6dff00a45cb13383c360346a4
Christos Siannas <xrsia@otenet.gr>
2026-04-21 10:26:18 +0300

    search in a text file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 use std::fs;
 
 fn main() {
-    let path = "./test_folder";
+    let content = fs::read_to_string("./test_folder/example.txt").unwrap();
 
-    let entries = fs::read_dir(path).unwrap();
-
-    for entry in entries {
-        let entry = entry.unwrap();
-        let file_path = entry.path();
-
-        // Convert path to string (so we can check it)
-        let path_str = file_path.to_string_lossy();
-
-        if path_str.ends_with(".pdf") {
-            println!("{:?}", file_path);
+    for line in content.lines() {
+        if line.contains("Rust") {
+            println!("Found: {}", line);
         }
     }
 }
