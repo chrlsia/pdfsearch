e772829fea223dd86e957247239c96e37c584998
Christos Siannas <xrsia@otenet.gr>
2026-04-21 14:23:13 +0300

    clap multiple words and directory

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
+    /// Words to search (1–3 words)
+    #[arg(short, long, num_args = 1..=3)]
+    words: Vec<String>,
+
+    /// Directory containing PDFs
     #[arg(short, long)]
-    name: String,
+    dir: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("Hello {}", args.name);
+    println!("Words: {:?}", args.words);
+    println!("Directory: {}", args.dir);
 }
+/*
+cargo r -q -- -w siannas chris -d ./chris
+
+Words: ["siannas", "chris"]
+Directory: ./chris
+*/
