6b2f29b839a545ff994bb7bbb47de48d6d93723b
Christos Siannas <xrsia@otenet.gr>
2026-04-21 13:50:59 +0300

    better example to separate words from folder

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,18 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    println!("{:?}", args);
+    if args.len() < 3 {
+        println!("Usage: cargo run <word1> [word2] [word3] <folder>");
+        return;
+    }
+
+    let folder = args.last().unwrap();
+
+    let words: Vec<String> = args[1..args.len() - 1]
+        .iter()
+        .map(|w| w.to_lowercase())
+        .collect();
+
+    println!("Folder: {}", folder);
+    println!("Words: {:?}", words);
 }
