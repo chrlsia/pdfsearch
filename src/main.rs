233fc6e29a0aa6d510c258b46bed2569b1d03335
Christos Siannas <xrsia@otenet.gr>
2026-04-21 13:39:36 +0300

    accept user input basic concepts

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,15 @@
-use std::fs;
-use pdf_extract::extract_text;
+use std::io;
 
 fn main() {
-    let folder = "./test_folder";
-    let query = "rust safe";
+    let mut query = String::new();
 
-    let words: Vec<String> = query
-        .split_whitespace()
-        .map(|w| w.to_lowercase())
-        .collect();
+    println!("Enter search words (1–3 words):");
 
-    let entries = fs::read_dir(folder).unwrap();
+    io::stdin()
+        .read_line(&mut query)
+        .unwrap();
 
-    for entry in entries {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let path_str = path.to_string_lossy();
+    let query = query.trim().to_string();
 
-        if path_str.ends_with(".pdf") {
-            let content = extract_text(&path).unwrap();
-
-            for (line_number, line) in content.lines().enumerate() {
-                let line_lower = line.to_lowercase();
-
-                let mut all_found = true;
-
-                for word in &words {
-                    if !line_lower.contains(word) {
-                        all_found = false;
-                        break;
-                    }
-                }
-
-                if all_found {
-                    println!(
-                        "Found in file: {:?} at line {} -> {}",
-                        path, line_number + 1, line
-                    );
-                }
-            }
-        }
-    }
+    println!("You searched for: {}", query);
 }
