68ae8628901ed446a117cdf761cf1c7ebfe38bf1
Christos Siannas <xrsia@otenet.gr>
2026-04-21 14:35:17 +0300

    minimal example of WalkDir

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,8 @@
-use clap::Parser;
-use pdf_extract::extract_text;
-use std::fs;
-
-#[derive(Parser)]
-struct Args {
-    /// Words to search (1–3 words)
-    #[arg(short, long, num_args = 1..=3)]
-    words: Vec<String>,
-
-    /// Directory containing PDFs
-    #[arg(short, long)]
-    dir: String,
-}
+use walkdir::WalkDir;
 
 fn main() {
-    let args = Args::parse();
-
-    let words: Vec<String> = args.words.iter().map(|w| w.to_lowercase()).collect();
-
-    let folder = args.dir;
-
-    if words.is_empty() || words.len() > 3 {
-        println!("Please provide 1 to 3 words.");
-        return;
-    }
-
-    let entries = fs::read_dir(folder).unwrap();
-
-    for entry in entries {
+    for entry in WalkDir::new("./test_folder") {
         let entry = entry.unwrap();
-        let path = entry.path();
-        let path_str = path.to_string_lossy();
-
-        if path_str.ends_with(".pdf") {
-            println!("Scanning file: {:?}...", path);
-
-            let content = extract_text(&path).unwrap();
-
-            for (line_number, line) in content.lines().enumerate() {
-                let line_lower = line.to_lowercase();
-
-                let mut all_found = true;
-
-                for word in &words {
-                    if !line_lower.contains(word) {
-                        all_found = false;
-                        break;
-                    }
-                }
-
-                if all_found {
-                    println!("\nFound in file: {:?} at line {}", path, line_number + 1);
-                    println!("> {}", line);
-                }
-            }
-        }
+        println!("{:?}", entry.path());
     }
 }
-
-/*
-
-cargo r -q -- -w rust safe -d ./test_folder
-Scanning file: "./test_folder/exa2.pdf"...
-
-Found in file: "./test_folder/exa2.pdf" at line 4
-> Rust is memory safe
-Scanning file: "./test_folder/Lesson+11+-+Time+Order+Words.pdf"...
-Scanning file: "./test_folder/exa1.pdf"...
-
-Found in file: "./test_folder/exa1.pdf" at line 4
-> Rust is memory safe
-Scanning file: "./test_folder/example.pdf"...
-
-Found in file: "./test_folder/example.pdf" at line 4
-> Rust is memory safe
-*/
