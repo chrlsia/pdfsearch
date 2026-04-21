a84a68e4ec288dcf44a5a424cf0a9c7bbbc25783
Christos Siannas <xrsia@otenet.gr>
2026-04-21 08:56:07 +0300

    read and display the contents of test_folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "./test_folder";
+
+    let entries = fs::read_dir(path).unwrap();
+
+    for entry in entries {
+        let entry = entry.unwrap();
+        let file_path = entry.path();
+
+        println!("{:?}", file_path);
+    }
 }
