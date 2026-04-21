ad82159a9b6b2d6f6fb7c2e33754a7405df8c9cd
Christos Siannas <xrsia@otenet.gr>
2026-04-21 13:17:57 +0300

    small useful example to extract txt from pdf file

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,7 @@
-use std::fs;
+use pdf_extract::extract_text;
 
 fn main() {
-    let folder = "./test_folder";
+    let text = extract_text("./test_folder/example.pdf").unwrap();
 
-    let entries = fs::read_dir(folder).unwrap();
-
-    for entry in entries {
-        let entry = entry.unwrap();
-        let path = entry.path();
-
-        let path_str = path.to_string_lossy();
-
-        // For now: use .txt instead of .pdf
-        if path_str.ends_with(".txt") {
-            let content = fs::read_to_string(&path).unwrap();
-
-            let query = "Rust safe"; // later this will come from user
-
-            let words: Vec<String> = query.split_whitespace().map(|w| w.to_lowercase()).collect();
-
-            for (line_number, line) in content.lines().enumerate() {
-                let line_lower=line.to_lowercase();
-                let mut all_found = true;
-
-                for word in &words {
-                    if !line_lower.contains(word) {
-                        all_found = false;
-                        break;
-                    }
-                }
-
-                if all_found {
-                    println!(
-                        "Found in file: {:?} at line {} -> {}",
-                        path,
-                        line_number + 1,
-                        line
-                    );
-                }
-            }
-        }
-    }
+    println!("{}", text);
 }
