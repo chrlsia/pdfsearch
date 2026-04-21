d6ce59466410cda06d94c1c3934f05b50e10971f
Christos Siannas <xrsia@otenet.gr>
2026-04-21 10:50:58 +0300

    find 3 words, modified version of tag find 2 words

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,26 @@ fn main() {
         if path_str.ends_with(".txt") {
             let content = fs::read_to_string(&path).unwrap();
 
+            let query = "Rust safe"; // later this will come from user
+
+            let words: Vec<&str> = query.split_whitespace().collect();
+
             for (line_number, line) in content.lines().enumerate() {
-                if line.contains("Rust") && line.contains("safe") {
+                let mut all_found = true;
+
+                for word in &words {
+                    if !line.contains(word) {
+                        all_found = false;
+                        break;
+                    }
+                }
+
+                if all_found {
                     println!(
                         "Found in file: {:?} at line {} -> {}",
-                        path, line_number + 1, line
+                        path,
+                        line_number + 1,
+                        line
                     );
                 }
             }
