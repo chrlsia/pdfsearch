db5022823ed4a58d07b416d01484bb6e857d5c99
Christos Siannas <xrsia@otenet.gr>
2026-04-21 14:30:33 +0300

    final version with clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use pdf_extract::extract_text;
+use std::fs;
 
 #[derive(Parser)]
 struct Args {
@@ -14,12 +16,44 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    println!("Words: {:?}", args.words);
-    println!("Directory: {}", args.dir);
-}
-/*
-cargo r -q -- -w siannas chris -d ./chris
+    let words: Vec<String> = args.words.iter().map(|w| w.to_lowercase()).collect();
 
-Words: ["siannas", "chris"]
-Directory: ./chris
-*/
+    let folder = args.dir;
+
+    if words.is_empty() || words.len() > 3 {
+        println!("Please provide 1 to 3 words.");
+        return;
+    }
+
+    let entries = fs::read_dir(folder).unwrap();
+
+    for entry in entries {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let path_str = path.to_string_lossy();
+
+        if path_str.ends_with(".pdf") {
+            println!("Scanning file: {:?}...", path);
+
+            let content = extract_text(&path).unwrap();
+
+            for (line_number, line) in content.lines().enumerate() {
+                let line_lower = line.to_lowercase();
+
+                let mut all_found = true;
+
+                for word in &words {
+                    if !line_lower.contains(word) {
+                        all_found = false;
+                        break;
+                    }
+                }
+
+                if all_found {
+                    println!("\nFound in file: {:?} at line {}", path, line_number + 1);
+                    println!("> {}", line);
+                }
+            }
+        }
+    }
+}
