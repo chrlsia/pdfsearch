ece232d59ee4055475f1e3101d11ef9cda8d727d
Christos Siannas <xrsia@otenet.gr>
2026-04-21 10:32:06 +0300

    search in a folder, in all files, for the word Rust, display results

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("./test_folder/example.txt").unwrap();
+    let folder = "./test_folder";
 
-    for line in content.lines() {
-        if line.contains("Rust") {
-            println!("Found: {}", line);
+    let entries = fs::read_dir(folder).unwrap();
+
+    for entry in entries {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        let path_str = path.to_string_lossy();
+
+        // For now: use .txt instead of .pdf
+        if path_str.ends_with(".txt") {
+            let content = fs::read_to_string(&path).unwrap();
+
+            for (line_number, line) in content.lines().enumerate() {
+                if line.contains("Rust") {
+                    println!(
+                        "Found in file: {:?} at line {} -> {}",
+                        path, line_number + 1, line
+                    );
+                }
+            }
         }
     }
 }
